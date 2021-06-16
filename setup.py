899d968d04ece41dfcdf29469d77bb91a6f9e690
z14git <lzl1992@gmail.com>
2021-06-16 16:57:38 +0800

    bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jetcam',
-    version='0.1.0',
+    version='0.2.0',
     description='An easy to use camera interface for NVIDIA Jetson',
     packages=find_packages(),
     install_requires=[
